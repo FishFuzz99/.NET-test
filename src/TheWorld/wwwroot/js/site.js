95a4ad527e138865b78aec17c4cb60c451974e56
FishFuzz99 <fishfuzz99@gmail.com>
2015-11-24 17:54:14 -0700

    Got bower working

---
+++ src/TheWorld/wwwroot/js/site.js
@@ -0,0 +1,13 @@
+﻿// site.js
+(function startup() {
+    var element = document.getElementById("username");
+    element.innerHTML = "Andrew Gray";
+
+    var main = document.getElementById("main");
+    main.onmouseenter = function () {
+        main.style = "background-color: #888;";
+    };
+    main.onmouseleave = function () {
+        main.style = "";
+    };
+})();
